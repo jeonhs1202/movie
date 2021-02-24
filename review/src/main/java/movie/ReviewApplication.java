31ff96ced7d1d233dc14635dcf55a656ffb2f36d
root <jeonhs1202@sk.com>
2021-02-24 21:10:47 +0900

    hy

--- review/src/main/java/movie/ReviewApplication.java
+++ review/src/main/java/movie/ReviewApplication.java
@@ -5,15 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableBinding(KafkaProcessor.class)
 @EnableFeignClients
-@EnableCircuitBreaker
-@EnableHystrixDashboard
 
 public class ReviewApplication {
     protected static ApplicationContext applicationContext;
